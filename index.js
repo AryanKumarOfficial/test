76bd6113f23e12c0ee75ff544a96f3a090d22715
Aryan Kumar <aryanak9163@gmail.com>
2024-10-20 00:49:31 +0530

    uses environment variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import "dotenv/config"
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json()); // accepts json requests
 
 let teaData = [];
@@ -57,5 +58,5 @@ app.delete("/teas/:id", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on http://${`127.0.0.1`}${port}`);
+    console.log(`Server is running on http://${`127.0.0.1`}:${port}`);
 })
